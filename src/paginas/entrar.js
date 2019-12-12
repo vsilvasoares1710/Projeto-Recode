d9a24a64cea77bb875744c1a20a008acb3a2373a
vsilvasoares1710 <vsilvasoares1710@gmail.com>
2019-12-12 11:09:17 -0300

    rotas estabelecidas

---
+++ src/paginas/entrar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+class Entrar extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1>Entrar</h1>
+					<Link to='/'>
+						<button>
+							Home Page
+            			</button>
+					</Link >
+					<Link to='/encontreProfissionais'>
+						<button>
+							Encontre Profissionais
+						</button>
+					</Link >
+					<Link to='/divulgueSeuTrabalho'>
+						<button>
+							Divulgue Seu Trabalho
+						</button>
+					</Link >
+					<Link to='/contato'>
+						<button>
+							Contato
+						</button>
+					</Link >
+					<Link to='/quemSomos'>
+						<button>
+							Quem Somos
+						</button>
+					</Link >
+					<Link to='/entrar'>
+						<button>
+							Entrar
+						</button>
+					</Link >
+					<Link to='/cadastro'>
+						<button>
+							Cadastro
+						</button>
+					</Link >
+				</header>
+			</div>
+		);
+	}
+
+}
+
+export default Entrar;
---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+//components
+import Home from './paginas/home'
+import EncontreProfissionais from './paginas/encontreProfissionais'
+import DivulgueSeuTrabalho from './paginas/divulgueSeuTrabalho'
+import Contato from './paginas/contato'
+import QuemSomos from './paginas/quemSomos'
+import Entrar from './paginas/entrar'
+import Cadastro from './paginas/cadastro'
+
+function Routes() {
+	return (
+		<Switch>
+			<Route exact path='/' component={Home} />
+			<Route exact path='/encontreProfissionais' component={EncontreProfissionais} />
+			<Route exact path='/divulgueSeuTrabalho' component={DivulgueSeuTrabalho} />
+			<Route exact path='/contato' component={Contato} />
+			<Route exact path='/quemSomos' component={QuemSomos} />
+			<Route exact path='/entrar' component={Entrar} />
+			<Route exact path='/cadastro' component={Cadastro} />
+		</Switch>
+	)
+
+}
+
+export default Routes;
